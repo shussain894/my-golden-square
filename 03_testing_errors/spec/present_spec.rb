5a514137f8062afb93afc378a71cdf8d4bcbece7
Shahrukh Hussain <shah@MASTUDENT-Shahrukh-Hussain-C02G57C0Q6L4.local>
2022-11-08 16:24:55 +0000

    answer for present.rb updated in full

--- 03_testing_errors/spec/present_spec.rb
+++ 03_testing_errors/spec/present_spec.rb
@@ -19,4 +19,15 @@ RSpec.describe Present do
       expect { present.wrap("astronaut") }.to raise_error "A contents has already been wrapped."
     end
   end
+  context "when no contents have been wrapped" do
+    it "fails" do
+      present = Present.new
+      expect { present.unwrap }.to raise_error "No contents have been wrapped."
+    end
+  end
+    it "unwraps present" do
+      present = Present.new
+      present.wrap("rocket")
+      expect(present.unwrap).to eq "rocket"
+    end 
 end
