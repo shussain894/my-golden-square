5e765da98d03788b6e5751361f88e8fd9e69be6a
Shahrukh Hussain <shahhussain1@me.com>
2022-11-10 15:03:45 +0000

    deleted duplicate folders

--- 03_testing_bites_errors/lib/password_checker.rb
+++
@@ -1,9 +0,0 @@
-class PasswordChecker
-    def check(password)
-      if password.length >= 8
-        return true
-      else
-        fail "Invalid password, must be 8+ characters."
-      end
-    end
-end
